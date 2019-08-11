635b888ae0a4a969cdbe10b3a312d9d756af4733
DESKTOP-LBSS1Q2\jakeJoestar <jakefwilliams1994@gmail.com>
2019-08-11 16:06:53 +0100

    Adding initial commit to keep track of current task

---
+++ TimeLogger/TimeLogger.Core.Tests/LoggerTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TimeLogger.Core.Tests
+{
+    public class LoggerTests
+    {
+        [Test]
+        public void Log_WithDateTime_SetsCurrentTask()
+        {
+            var logger = new Logger();
+            logger.Log("current-task");
+            logger.CurrentTask.Should().BeEquivalentTo("current-task");
+        }
+    }
+}
---
+++ TimeLogger/TimeLogger.Core/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TimeLogger.Core
+{
+    public class Logger
+    {
+        public string CurrentTask { get; set; }
+
+        public void Log(string task)
+        {
+            CurrentTask = task;
+        }
+    }
+}
