2aab03d0c4fbc68469fdfb18089594a6ae76eaae
DESKTOP-LBSS1Q2\jakeJoestar <jakefwilliams1994@gmail.com>
2019-08-11 16:09:55 +0100

    updating method name to better reflect its purpose

--- TimeLogger/TimeLogger.Core.Tests/LoggerTests.cs
+++ TimeLogger/TimeLogger.Core.Tests/LoggerTests.cs
@@ -6,10 +6,10 @@ namespace TimeLogger.Core.Tests
     public class LoggerTests
     {
         [Test]
-        public void Log_WithDateTime_SetsCurrentTask()
+        public void SetCurrentTask_WithDateTime_SetsCurrentTask()
         {
             var logger = new Logger();
-            logger.Log("current-task");
+            logger.SetCurrentTask("current-task");
             logger.CurrentTask.Should().BeEquivalentTo("current-task");
         }
     }
--- TimeLogger/TimeLogger.Core/Logger.cs
+++ TimeLogger/TimeLogger.Core/Logger.cs
@@ -6,7 +6,7 @@ namespace TimeLogger.Core
     {
         public string CurrentTask { get; set; }
 
-        public void Log(string task)
+        public void SetCurrentTask(string task)
         {
             CurrentTask = task;
         }
